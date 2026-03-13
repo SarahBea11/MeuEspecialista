7812d98cd6428ee63231a35fb684f67764c9b5b6
matadorxfamoso <matadorxfamoso@gmail.com>
2026-03-13 01:48:01 -0300

    adicionando a pasta services para a comunicação com o php e pequenos ajustes

--- angular-frontend/src/app/app-module.ts
+++ angular-frontend/src/app/app-module.ts
@@ -1,5 +1,7 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; 
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
@@ -7,9 +9,20 @@ import { Login } from './login/login';
 import { Cadastro } from './cadastro/cadastro';
 
 @NgModule({
-  declarations: [App, Login],
-  imports: [BrowserModule, AppRoutingModule,Cadastro],
-  providers: [provideBrowserGlobalErrorListeners()],
-  bootstrap: [App],
+  declarations: [
+    App,
+    Login,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    Cadastro
+
+  ],
+  providers: [],
+  bootstrap: [App]
 })
-export class AppModule {}
+export class AppModule { }
--- angular-frontend/src/app/login/login.ts
+++ angular-frontend/src/app/login/login.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,11 +15,19 @@ loginData = {
 
   }
 
+constructor(private authService: AuthService, private router: Router) {}
 
-constructor(){}
-
-onLogin() {
-  console.log(this.loginData);
+  logar() {
+    this.authService.login(this.loginData).subscribe({
+      next: (res) => {
+        console.log('Sucesso!', res);
+        alert('Login realizado!');
+        this.router.navigate(['/cadastro']); // Redireciona após o login
+      },
+      error: (err) => {
+        console.error('Erro!', err);
+        alert('E-mail ou senha incorretos.');
+      }
+    });
   }
-
 }
