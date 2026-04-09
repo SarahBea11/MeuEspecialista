3fe6a541e09aba2a24e69a5afa5566142cdac156
SarahBea11 <sarahbealimapina@gmail.com>
2026-04-09 16:02:15 -0300

    feat: adiciona componente Home com estrutura HTML e estilos CSS

--- angular-frontend/src/app/app-module.ts
+++ angular-frontend/src/app/app-module.ts
@@ -7,9 +7,10 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Login } from './login/login';
 import { Cadastro } from './cadastro/cadastro';
+import { Home } from './home/home';
 
 @NgModule({
-  declarations: [App, Login],
+  declarations: [App, Login, Home],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, Cadastro],
   providers: [],
   bootstrap: [App],
--- angular-frontend/src/app/app-routing-module.ts
+++ angular-frontend/src/app/app-routing-module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Cadastro } from './cadastro/cadastro';
+import { Home } from './home/home';
 
 export const routes: Routes = [
-  { path: '', component: Login },
+  { path: 'login', component: Login },
   { path: 'cadastro', component: Cadastro },
+  { path: '', component: Home }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
