22670a64a0920d0d08c317700d05f01347b494bc
SarahBea11 <sarahbealimapina@gmail.com>
2026-04-15 21:16:41 -0300

    fix: remove interferência de corretor na navbar

--- angular-frontend/src/app/buscar/buscar.ts
+++ angular-frontend/src/app/buscar/buscar.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-buscar',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './buscar.html',
   styleUrl: './buscar.css',
 })
